fa30cb5d051229207b44afea2ec5ac5cb3177ba5
Aritra Das <aritradas00@gmail.com>
2026-07-07 02:00:21 +0530

    Initial Commit

---
+++ src/Basic.java
@@ -0,0 +1,22 @@
+//public class Basic {
+//}
+
+void main(String[] args){
+    System.out.println("Hello World" + " Aritra"); // Printing String
+    System.out.print(3);                           // Printing Integer
+    System.out.println(4+3);                       // Printing Sum
+    System.out.println("4+3");                     // Printing the expression as a String
+
+
+
+//    Lorem Ipsum is simply dummy text of the printing and t
+//    ypesetting industry. Lorem Ipsum has been the industry's
+//    standard dummy text ever since 1966, when designers at
+//    Letraset and James Mosley, the librarian at St Bride Printing Library i\
+    System.out.println("4+3");
+    /*n London, took a 1914 Cicero translation and scrambled it to make dummy text
+    for Letraset's Body Type sheets. It has survived not only many decades, but
+    also the leap into electronic typesetting, remaining essentially unchanged.
+    It was popularised thanks to these sheets and more recently with desktop.*/
+    System.out.println("4+3");
+}
