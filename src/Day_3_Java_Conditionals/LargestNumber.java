e28ce7b8f08bfceb2efe6f0ac0974dca34b9f1bb
Aritra Das <aritradas00@gmail.com>
2026-07-11 00:33:33 +0530

    Practice Program Largest Number

---
+++ src/Day_3_Java_Conditionals/LargestNumber.java
@@ -0,0 +1,29 @@
+package Day_3_Java_Conditionals;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the First Number: ");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter the second Number: ");
+        int secondNumber = input.nextInt();
+        System.out.println("Enter the third Number: ");
+        int thirdNumber = input.nextInt();
+
+        if (firstNumber > secondNumber) {
+            if (firstNumber > thirdNumber) {
+                System.out.println("First Number: " + firstNumber + " is the Largest Number.");
+            } else {
+                System.out.println("Third Number: " + thirdNumber + " is the Largest Number.");
+            }
+        } else if (secondNumber > thirdNumber) {
+            System.out.println("Second Number: " + secondNumber + " is the Largest Number.");
+        } else {
+            System.out.println("Third Number: " + thirdNumber + " is the Largest Number.");
+        }
+        input.close();
+    }
+}
